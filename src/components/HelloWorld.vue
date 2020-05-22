bb920d8fc22dc4cea7b0e1d78da62f05fefc3123
guilherme santos <guilhermess1994@gmail.com>
2020-05-22 10:24:18 -0300

    initial implementation with face detection

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,114 +1,96 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <video
+      ref="video"
+      width="400"
+      height="400"
+      playsinline
+      muted
+      autoplay
+    ></video>
+    <canvas ref="canvas" width="400" height="400"></canvas>
   </div>
 </template>
 
 <script>
+import * as faceapi from "face-api.js";
 export default {
   name: "HelloWorld",
+
   props: {
     msg: String
+  },
+
+  data: () => ({
+    video: {},
+    canvas: {},
+    captures: []
+  }),
+
+  methods: {
+    startVideo() {
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then(stream => {
+          this.$refs.video.srcObject = stream;
+          this.$refs.video.camonloadedmetadata = () => {
+            this.$refs.video.play();
+          };
+          this.detection();
+        })
+        .catch(err => console.error(err));
+    },
+
+    detection() {
+      console.log("detection starts");
+      const displaySize = {
+        width: this.$refs.video.width,
+        height: this.$refs.video.height
+      };
+      faceapi.matchDimensions(this.$refs.canvas, displaySize);
+
+      setInterval(async () => {
+        const detections = await faceapi
+          .detectAllFaces(
+            this.$refs.video,
+            new faceapi.TinyFaceDetectorOptions()
+          )
+          .withFaceExpressions();
+        const resizedDetections = faceapi.resizeResults(
+          detections,
+          displaySize
+        );
+        this.$refs.canvas.getContext("2d").clearRect(0, 0, 640, 480);
+        faceapi.draw.drawDetections(this.$refs.canvas, resizedDetections);
+        faceapi.draw.drawFaceExpressions(this.$refs.canvas, resizedDetections);
+      }, 200);
+    },
+
+    loadfaceapi() {
+      Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+        faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+        faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+        faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+        faceapi.nets.ageGenderNet.loadFromUri("/models")
+      ])
+        .then(this.startVideo())
+        .catch(err => console.error(err));
+    }
+  },
+  mounted() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      this.loadfaceapi();
+    }
   }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+canvas,
+video {
+  position: absolute;
+  top: 0;
+  left: 0;
 }
 </style>
